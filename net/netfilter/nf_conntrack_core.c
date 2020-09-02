552f98280fc6f7f58a4650a210df607c299c8ee5
BlackMesa123 <brother12@hotmail.it>
2020-09-02 11:11:45 +0300

    net: disableable KNOX net implementation

--- net/netfilter/nf_conntrack_core.c
+++ net/netfilter/nf_conntrack_core.c
@@ -58,9 +58,11 @@
 #include <net/netns/hash.h>
 
 #include "nf_internals.h"
+#ifdef CONFIG_KNOX_NCM
 // KNOX NPA - START
 #include <net/ncm.h>
 // KNOX NPA - END
+#endif
 
 #define NF_CONNTRACK_VERSION	"0.5.0"
 
@@ -361,6 +363,7 @@ static void nf_ct_add_to_dying_list(struct nf_conn *ct)
 {
 	struct ct_pcpu *pcpu;
 
+#ifdef CONFIG_KNOX_NCM
 	// KNOX NPA - START
 	/* Add 'del_timer(&ct->npa_timeout)' if struct nf_conn->timeout is of type struct timer_list; */
 	/* send dying conntrack entry to collect data */
@@ -368,6 +371,7 @@ static void nf_ct_add_to_dying_list(struct nf_conn *ct)
 		knox_collect_conntrack_data(ct, NCM_FLOW_TYPE_CLOSE, 10);
 	}
 	// KNOX NPA - END
+#endif
 
 	/* add this conntrack to the (per cpu) dying list */
 	ct->cpu = smp_processor_id();
@@ -1128,6 +1132,7 @@ static void gc_worker(struct work_struct *work)
 			if (nf_ct_is_expired(tmp)) {
 				nf_ct_gc_expired(tmp);
 				continue;
+#ifdef CONFIG_KNOX_NCM
 			// KNOX NPA - START
 			} else if ( (tmp != NULL) && (check_ncm_flag()) && (check_intermediate_flag()) && (atomic_read(&tmp->startFlow)) && (atomic_read(&tmp->intermediateFlow)) ) {
 				s32 npa_timeout = tmp->npa_timeout - ((u32)(jiffies));
@@ -1135,6 +1140,7 @@ static void gc_worker(struct work_struct *work)
 					tmp->npa_timeout = ((u32)(jiffies)) + (get_intermediate_timeout() * HZ);
 					knox_collect_conntrack_data(tmp, NCM_FLOW_TYPE_INTERMEDIATE, 20);
 				}
+#endif
 			}
 			// KNOX NPA - END
 
@@ -1206,9 +1212,11 @@ __nf_conntrack_alloc(struct net *net,
 		     gfp_t gfp, u32 hash)
 {
 	struct nf_conn *ct;
+#ifdef CONFIG_KNOX_NCM
 	// KNOX NPA - START
 	struct timespec open_timespec;
 	// KNOX NPA - END
+#endif
 
 	/* We don't want any race condition at early drop stage */
 	atomic_inc(&net->ct.count);
@@ -1234,6 +1242,7 @@ __nf_conntrack_alloc(struct net *net,
 
 	spin_lock_init(&ct->lock);
 
+#ifdef CONFIG_KNOX_NCM
 	// KNOX NPA - START
 	/* initialize the conntrack structure members when memory is allocated */
 	if (ct != NULL) {
@@ -1256,6 +1265,7 @@ __nf_conntrack_alloc(struct net *net,
 		atomic_set(&ct->intermediateFlow, 0);
 	}
 	// KNOX NPA - END
+#endif
 
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].tuple = *orig;
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].hnnode.pprev = NULL;
